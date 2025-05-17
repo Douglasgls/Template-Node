4b804f920c1ebf98fe07c334f075be400a09e65a
Douglas Paz <douglaspaz955@gmail.com>
2025-05-17 17:55:26 -0300

    Exemplo do arq .env

--- pix/teste.js
+++ pix/teste.js
@@ -4,7 +4,7 @@ import PixService  from './PixService.js';
 const authService = new AuthService();
 const pixService = new PixService(authService);
 
-const cobranca = await pixService.setCobPix(3600, '16147287478', 'douglas paz da silva', '0.01');
+const cobranca = await pixService.setCobPix(3600, '12345678909', 'douglas paz da silva', '0.01');
 
 const token = await authService.getValidToken(); 
 const qrCode = await pixService.getQrCode(cobranca.loc.id,token);
